b9b4253d017c513bfb8af6d5d823ff76e1ed7b99
Rohith Reddy <67363894+rohithmada00@users.noreply.github.com>
2020-08-30 18:15:37 +0530

    Add files via upload

---
+++ kahaani/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib import admin
+ 
+from django.contrib.auth import views
+ 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('accounts/login/', views.login, name='login'),
+    #path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')),
+]
